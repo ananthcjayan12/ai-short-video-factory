3f4182d1d46d3ac52040779ccb21deab9c9e2f57
ananthcjayan12 <123536374+ananthcjayan12@users.noreply.github.com>
2026-08-19 23:01:07 +0530

    remove obsolete editorial layout monkeypatch

--- shorts_factory/__init__.py
+++ shorts_factory/__init__.py
@@ -5,9 +5,3 @@ from .editorial_orchestrator import register_editorial_tasks
 register_editorial_tasks()
 
 del register_editorial_tasks
-
-from .editorial_layout_validation import install_repairable_sequence_layout_validation
-
-install_repairable_sequence_layout_validation()
-
-del install_repairable_sequence_layout_validation
