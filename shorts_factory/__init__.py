8eb6ec597bc50071ac41d8a9ecbbf1819c7203b5
ananthcjayan12 <123536374+ananthcjayan12@users.noreply.github.com>
2026-08-19 23:31:04 +0530

    install V3 provider schema adapter

--- shorts_factory/__init__.py
+++ shorts_factory/__init__.py
@@ -5,3 +5,9 @@ from .editorial_orchestrator import register_editorial_tasks
 register_editorial_tasks()
 
 del register_editorial_tasks
+
+from .editorial_v3_schema import install_v3_provider_schema
+
+install_v3_provider_schema()
+
+del install_v3_provider_schema
